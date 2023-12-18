77f4f85b1be0d17c9bde1ef683613861319d6291
June" GOMBERT <114985594+gombert-j@users.noreply.github.com>
2023-12-18 09:59:04 +0100

    Tags: Changement de la syntaxe du nom des méthodes

--- Classes/Tags.py
+++ Classes/Tags.py
@@ -3,7 +3,7 @@ class TagsManager:
     def __init__(self):
         self.__liste_tags = list()
 
-    def newTag(self, new_tag : str):
+    def new_tag(self, new_tag : str):
         liste = self.getTags()
         for tag in liste:
             if(new_tag != tag): 
@@ -11,9 +11,9 @@ class TagsManager:
                 print(f"{new_tag} ajouté avec succès")
             else: print(f"{tag} déjà existant")
 
-    def getTags(self):
+    def inspect_tags(self):
         Choix.lister_options(self.__liste_tags)
 
 class Tags:
-    def setName(self, nom : str):
+    def set_name(self, nom : str):
         self.__nomTag = nom
