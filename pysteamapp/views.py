591fde01ff76402cb1457b6dc70bdfc937d37da5
June" GOMBERT <114985594+gombert-j@users.noreply.github.com>
2024-02-22 15:21:10 +0100

    Base conversion des classes en models

--- pysteamapp/views.py
+++ pysteamapp/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Index of shared list')
+
+# new_game()
+def new_game(request):
+    shopping_list = ShopList.objects.all()
+    # template loader
+    template = loader.get_template('shoplist.html')
+    # context
+    context = { 'shoplists': shopping_list}
+    return HttpResponse(template.render(context, request))
