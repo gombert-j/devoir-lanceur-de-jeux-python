ad78f49bf40bddb59d26548f756685d3d445594b
June" GOMBERT <114985594+gombert-j@users.noreply.github.com>
2024-02-22 16:58:27 +0100

    Déplacement de la racine

--- application.py
+++
@@ -1,4 +0,0 @@
-from Classes.JeuxBibliotheque import Bibliotheque
-if __name__ == '__main__':
-    pysteam = Bibliotheque("pysteam-library.json")
-    pysteam.afficher_menu()
--- pysteamproject/pysteamapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PysteamappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pysteamapp'
