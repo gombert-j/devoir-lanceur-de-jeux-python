d0a2d2a221e348253bec1c375233c2bf4603c4c5
June Gombert <junegombert@murena.io>
2024-03-02 23:24:19 +0100

    Test views fixed

--- pysteamapp/testviews.py
+++ pysteamapp/testviews.py
@@ -12,10 +12,10 @@ class ViewsTest(TestCase):
         response = self.client.get('/')
         self.assertEqual(200, response.status_code)
 
-    def test_une_collection(self):
+    def test_au_moins_une_collection(self):
         response = self.client.get('/')
         soup = BeautifulSoup(response.content, 'html.parser')
-        self.assertEqual(1, len(soup.find_all('li')))
+        self.assertLessEqual(1, len(soup.find_all('li')))
 
     def test_collection_contient_jeu(self):
         response = self.client.get('/bibliotheques/1')
